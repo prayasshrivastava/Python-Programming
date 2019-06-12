dd7b125ac1ac44024b3cef2d2d68ecec5f0ff409
Prayas shrivastava <prayasshrivastava118@gmail.com>
2019-06-13 02:57:25 +0530

    Update Name&age.py

--- Name&age.py
+++ Name&age.py
@@ -1,3 +1,8 @@
+# Create a program that asks the user to enter their name and their age
+#Print out a message that will tell them the year that they will turn 95 years old.
+
+#!/usr/bin/python3
+
 import datetime
 name=input("Enter your name: ")
 age =int(input("Enter your age: "))
