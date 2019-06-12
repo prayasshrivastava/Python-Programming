52c04a0aa8dd623227b0a4a84c80bb8789f24008
Prayas shrivastava <prayasshrivastava118@gmail.com>
2019-06-13 02:35:32 +0530

    Update List.py

--- List.py
+++ List.py
@@ -1,10 +1,10 @@
 adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
 list1=[]
 list2=[]
-for i in ad:
+for i in adhoc:
     if i>5:
         list1.append(i)
-for i in ad:
+for i in adhoc:
     if i<=2:
         list2.append(i)
 print("Numbers which are greater than 5:")
