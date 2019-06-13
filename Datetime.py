3540b7ba34a4cfcf7913793cf3f3db3360b3732d
Prayas shrivastava <prayasshrivastava118@gmail.com>
2019-06-13 09:47:00 +0530

    Update Datetime.py

--- Datetime.py
+++ Datetime.py
@@ -1,5 +1,7 @@
 #write a code  will take  input as your name and greet you with
-#good morning , good evening , goodafter noon , good night as per the current time your system :
+#good morning , good evening , goodafter noon , good night as per the current time your system 
+
+#!/usr/bin/python3
 
 import datetime
 
