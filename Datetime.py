38319b0d2b62e0a6a2fc8b2bfd328b580c5e45b9
Prayas shrivastava <prayasshrivastava118@gmail.com>
2019-06-13 09:35:05 +0530

    Create Datetime.py

---
+++ Datetime.py
@@ -0,0 +1,17 @@
+import datetime
+
+currentTime=datetime.datetime.now()
+currentTime.hour
+
+print("Enter your name:")
+x=input()
+
+
+if(currentTime.hour>=6 and currentTime.hour < 12):
+    print('Hii'+'' + x+'' + 'Good morning')
+elif(currentTime.hour >=12 and currentTime.hour<18):
+    print('Hii '+ x + 'Good afternoon')
+elif(currentTime.hour >=18 and currentTime.hour<21):
+    print('Hii'+'' + x +''+ 'GOOD evening')
+else:
+    print('Hii'+'' + x+'' + 'Good night')    
