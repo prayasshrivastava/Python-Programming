d314b31247cfaff1b8cc5ad61182a9d7de5c2f10
Prayas shrivastava <prayasshrivastava118@gmail.com>
2019-06-13 09:39:24 +0530

    Update Datetime.py

--- Datetime.py
+++ Datetime.py
@@ -1,3 +1,6 @@
+#write a code  will take  input as your name and greet you with
+#good morning , good evening , goodafter noon , good night as per the current time your system :
+
 import datetime
 
 currentTime=datetime.datetime.now()
@@ -8,10 +11,10 @@ x=input()
 
 
 if(currentTime.hour>=6 and currentTime.hour < 12):
-    print('Hii'+'' + x+'' + 'Good morning')
+    print('Hii'+ x + 'Good morning')
 elif(currentTime.hour >=12 and currentTime.hour<18):
     print('Hii '+ x + 'Good afternoon')
 elif(currentTime.hour >=18 and currentTime.hour<21):
-    print('Hii'+'' + x +''+ 'GOOD evening')
+    print('Hii' + x + 'GOOD evening')
 else:
-    print('Hii'+'' + x+'' + 'Good night')    
+    print('Hii'+'' + x+ 'Good night')    
