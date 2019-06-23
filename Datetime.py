7b054a97b43f557eb4b43e7acb180f101b46d90e
Prayas shrivastava <prayasshrivastava118@gmail.com>
2019-06-23 23:55:35 +0530

    Update Datetime.py

--- Datetime.py
+++ Datetime.py
@@ -1,4 +1,4 @@
-#write a code  will take  input as your name and greet you with
+#write a code  will take  input as your name and greet you 
 #good morning , good evening , goodafter noon , good night as per the current time your system 
 
 #!/usr/bin/python3
